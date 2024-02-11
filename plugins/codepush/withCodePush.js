3a2217f6f0039a8fab94c6eff9ab40eac0209fe6
Hailey <me@haileyok.com>
2024-02-11 13:40:50 -0800

    plist config plugin

--- plugins/codepush/withCodePush.js
+++ plugins/codepush/withCodePush.js
@@ -1,3 +1,18 @@
 const {withPlugins} = require('@expo/config-plugins')
 const {withAppDelegate} = require('./withAppDelegate')
 const {withInfoPlist} = require('./withInfoPlist')
+
+const CODEPUSH_DEPLOYMENT_KEY = process.env.CODEPUSH_DEPLOYMENT_KEY
+
+const withCodePush = config => {
+  return withPlugins(config, [
+    [
+      withInfoPlist,
+      {
+        deploymentKey: CODEPUSH_DEPLOYMENT_KEY,
+      },
+    ],
+  ])
+}
+
+module.exports = withCodePush
--- plugins/codepush/withInfoPlist.js
+++ plugins/codepush/withInfoPlist.js
@@ -1,3 +1,11 @@
-const withInfoPlist = (config, {codepushToken}) => {}
+const {withInfoPlist: withInfoPlistExpo} = require('@expo/config-plugins')
+
+const withInfoPlist = (config, {deploymentKey}) => {
+  // eslint-disable-next-line no-shadow
+  return withInfoPlistExpo(config, config => {
+    config.modResults.CodePushDeploymentKey = deploymentKey
+    return config
+  })
+}
 
 module.exports = {withInfoPlist}
